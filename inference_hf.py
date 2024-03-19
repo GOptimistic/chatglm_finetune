3b0826230ad3267d705424739e0d222b7b5d26d1
GOptimistic <969935802@qq.com>
2024-03-19 20:29:33 +0800

    inference

--- inference_hf.py
+++ inference_hf.py
@@ -3,6 +3,7 @@
 
 from pathlib import Path
 from typing import Annotated, Union
+import json
 
 import typer
 from peft import AutoPeftModelForCausalLM, PeftModelForCausalLM
@@ -42,14 +43,41 @@ def load_model_and_tokenizer(model_dir: Union[str, Path]) -> tuple[ModelType, To
     return model, tokenizer
 
 
+def write_to_txt(str_list, txt_path):
+    with open(txt_path, 'w', encoding='utf-8') as file:
+        # 将字符串数组逐行写入文件，并在每行末尾添加换行符
+        for line in str_list:
+            file.write(line + '\n')
+
+
 @app.command()
 def main(
         model_dir: Annotated[str, typer.Argument(help='')],
-        prompt: Annotated[str, typer.Option(help='')],
+        test_file: Annotated[str, typer.Option(help='')],
 ):
     model, tokenizer = load_model_and_tokenizer(model_dir)
-    response, _ = model.chat(tokenizer, prompt)
-    print(response)
+    print('Load model complete.')
+    prompt_list = []
+    summary_list = []
+    with open(test_file, 'r', encoding='utf-8') as file:
+        # 逐行读取数据
+        for line in file:
+            # 对每一行进行处理
+            # 例如，打印每一行内容
+            conversation = json.loads(line.strip())
+            prompt_list.append(conversation['conversations'][0]['content'])
+            summary_list.append(conversation['conversations'][1]['content'])
+    assert len(prompt_list) == len(summary_list)
+    pred_list = []
+    for i in range(len(prompt_list)):
+        prompt = prompt_list[i]
+        response, _ = model.chat(tokenizer, prompt)
+        print('Prompt {} is done. {}'.format(i, response))
+        pred_list.append(response)
+    print(len(pred_list))
+    write_to_txt(pred_list, './predictions/pred.txt')
+    write_to_txt(summary_list, './predictions/target.txt')
+    print('Inference done.')
 
 
 if __name__ == '__main__':
